aee27e37a7abccca67da4284ee12fae29679054f
sanjay <sanjaysundar2468@gmail.com>
2022-08-22 10:16:48 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+const posts =[
+    {
+    username:'user 1',
+    title:'post 1'
+},
+{
+    username:'user 2',
+    title:'post 2'
+}
+]
+let refreshTokens = []
+app.get('/posts',autenticateToken,(req,res)=>{
+    res.json(posts.filter(post=> post.username === req.user.name))
+    
+})
+
+app.post('/token',(req,res)=>{
+    const refreshToken = req.body.token
+    if(refreshToken==null) return res.sendStatus(401)
+    if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET,(err,user)=>{
+        if(err) return res.sendStatus(403)
+        const accessToken = generateAccessToken({name: user.name})
+        res.json({accessToken: accessToken})
+    })
+})
+
+app.post('/login',(req,res)=>{
+    const username = req.body.username
+    const user ={name: username}
+    const accessToken = generateAccessToken(user)
+    const refreshToken = jwt.sign(user,process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refreshToken)
+    res.json({accessToken: accessToken, refreshToken: refreshToken})
+})
+
+function generateAccessToken(user){
+    return jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'15s'})
+}
+function autenticateToken(req,res,next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token==null) return res.sendStatus(401)
+
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
+app.delete('/logout',(req,res)=>{
+    refreshTokens = refreshTokens.filter(token => token !==req.body.token)
+    res.sendStatus(204)
+})
+app.listen(3000)
